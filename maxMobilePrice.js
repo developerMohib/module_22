377ea0c7580fbbd7e7e0e9c1809301a6ad9dc5b2
Mohib <mohibullahmohim2020@gmail.com>
2024-02-07 01:23:22 +0600

    another task added

---
+++ maxMobilePrice.js
@@ -0,0 +1,21 @@
+let mobiles = [
+    {name:'sumsung', price : 20000, color : 'black', camera : '12 mp'},
+    {name:'oppo', price : 21000, color : 'black', camera : '12 mp'},
+    {name:'xiaomi', price : 22000, color : 'black', camera : '12 mp'},
+    {name:'vivo', price : 25000, color : 'black', camera : '12 mp'},
+    {name:'iphone', price : 33000, color : 'black', camera : '12 mp'},
+    {name:'lenevo', price : 24000, color : 'black', camera : '12 mp'}
+];
+
+function maxPrice(mobiles){
+    let max = mobiles[0];
+    for (const mobile of mobiles) {
+        if(mobile.price > max.price){
+            max = mobile;
+        }
+    }
+    return max;
+}
+let bestMobile = maxPrice(mobiles);
+document.write(' <br> Best Price Mobile : ', bestMobile, " check console for better known <br> ");
+console.log('Best Price Mobile : ', bestMobile, " <br> ");
