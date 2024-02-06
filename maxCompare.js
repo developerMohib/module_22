566719dde7dd1ee5e068e00f264b787c6d39cda6
Mohib <mohibullahmohim2020@gmail.com>
2024-02-07 00:41:27 +0600

    first commit

---
+++ maxCompare.js
@@ -0,0 +1,13 @@
+let heights = [78,75,74,84,90,89,88,98];
+
+function maxFider(heights){
+    let max = heights[0];
+    for (const height of heights) {
+        if(max < height){
+            max = height;
+        }
+    }
+    return max;
+}
+let maxHeight = maxFider(heights);
+document.write( 'Max Height from Array : ', maxHeight);
